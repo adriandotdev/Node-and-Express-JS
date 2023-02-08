8e96639c68b833e7db3ec7f2f392f1db5bf3692a
Adrian Nads <marcelonads@gmail.com>
2023-02-08 10:44:00 +0800

    UTILIZED MongoDB

--- app.js
+++ app.js
@@ -1,29 +1,66 @@
 const express = require('express');
-const kill = require('kill-port');
-
-// kill('3001', 'tcp')
-// .then(console.log("KILLED"))
-// .catch(console.log("NOT KILLED"));
+const path = require('path');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 const app = express();
 
+// Connection URI
+const dbURI = 'mongodb+srv://Lauriano_112300:lauriano_112300-expressjs@cluster0.2j2v3.mongodb.net/express-practice?retryWrites=true&w=majority';
+
+// Connect to the database
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => {
+        console.log('connected to db');
+        app.listen(3001, () => console.log("Express server listening to port 3001"));
+    })
+    .catch(err => console.log(err));
+// register view engine
+
+// USE THIS IF THE DEFAULT NAME OF FOLDER OF ALL TEMPLATES ARE 'views'
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+// WHILE USE THIS IF YOU WANT SPECIFIED NAME OF FOLDER
+// app.set('views', 'myviews');
+
 app.get('/', (req, res) => {
 
-    res.sendFile('./views/index.html', {root: __dirname});
+    res.redirect('/blogs');
+});
+
+app.get('/blogs', (req, res) => {
+
+    Blog.find().sort({ createdAt: -1 }).then(result => {
+
+        res.render('index', { title: 'Blogs', blogs: result}); // provide only the file withouth the extension.
+    }).catch(err => console.log(err));
+});
+
+app.post('/blogs', (req, res) => {
+
+    const blog = new Blog(req.body);
+
+    blog.save()
+        .then(result => { 
+            
+            console.log("Saved");
+            res.redirect('/blogs');
+        })
+        .catch(err => console.log("Error while saving occur"));
 });
 
 app.get('/about', (req, res) => {
 
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about', { title: 'About'});
 });
 
-app.get('/about-me', (req, res) => {
-    res.send("ABOUT ME PO ULIT")
+app.get('/create', (req, res) => {
+    res.render('create-blog', {title: 'Create'})
 });
 
 app.use((req, res) => {
 
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404', {title: 'Site Not Found'});
 });
 
-app.listen(3001, () => console.log("Express server listening to port 3001"));
